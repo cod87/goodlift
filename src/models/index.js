df00adca28196592afb216a4f6726d704341c937
CommsLRSD <communications@lrsd.net>
2025-11-19 14:37:49 -0600

    Delete src/models/index.js

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Models Index
- * 
- * Models for GoodLift application
- * Note: Workout planning functionality has been removed.
- * This file is kept for potential future model exports.
- */
-
-// Future models can be exported here
